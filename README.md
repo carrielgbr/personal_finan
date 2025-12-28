0e2fd69d23dce0c73fe00a88ece2ec00cb8de560
Allan Ferreira <afsn01@protonmail.com>
2025-12-28 17:36:27 -0300

    Kivy UI Test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Finansee
 
 Olá sou um projeto de gestão de controle financeiro.
+
+### Packages
+
+- kivy (Interface Gráfica)
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Hello, World!")
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class LoginScreen(Widget):
+    pass
+
+class FinanseeApp(App):
+    def build(self):
+        return LoginScreen()
+
+if __name__ == '__main__':
+    FinanseeApp().run()
