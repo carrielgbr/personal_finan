1ae864c2645a42b57519c36dc8887a2b44dfdcef
carrielgbr <gabrielcarriel61@gmail.com>
2026-01-11 14:21:15 -0300

    testadno troca de cor e troca de fonte no arquivo main.s

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-root.title("Welcome to GeekForGeeks")
+root.title("Finansee | Your Personal Finance Manager")
 root.geometry('1800x900')
+root.configure(background = 'light blue')
 
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
+lbl = Label(root, text = "Welcome to Finansee")
+lbl.config(font =("Courier", 44))
+lbl.pack(expand=True, fill='both')
+lbl.grid(column=700, row=400)
+
 
 # function to display text when
 # button is clicked
