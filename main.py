fbaaeb659ac3a39d610767c2019c37c107135b9d
carrielgbr <gabrielcarriel61@gmail.com>
2026-01-13 19:41:07 -0300

    refatorando o código do arquivo main.py

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title("Finansee | Your Personal Finance Manager")
-root.geometry('1800x900')
-root.configure(background = 'light blue')
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Simple Tkinter App")
 
-lbl = Label(root, text = "Welcome to Finansee")
-lbl.config(font =("Courier", 44))
-lbl.pack(expand=True, fill='both')
-lbl.grid(column=700, row=400)
+        self.label = Label(root, text="Hello, Tkinter!")
+        self.label.pack(pady=10)
 
+        self.button = Button(root, text="Click Me", command=self.on_button_click)
+        self.button.pack(pady=10)
 
-# function to display text when
-# button is clicked
-def clicked():
-    lbl.pack(text = "I just got clicked")
-    lbl.pack(expand=True, fill='both')
-# button widget with red color text
-# inside
-btn = Button(root, text = "Click me" ,
-             fg = "red", command=clicked)
-# set Button grid
-btn.grid(column=700, row=500)
+    def on_button_click(self):
+        self.label.config(text="Button Clicked!")
 
-# Execute Tkinter
-root.mainloop()
+    def tela(self):
+        self.root.configure(background='lightblue')
+
+if __name__ == "__main__":
+    root = Tk()
+    app = App(root)
+    root.mainloop()
