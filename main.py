27c77ca96c951dd199dc2e34677894cde777cdb3
carrielgbr <gabrielcarriel61@gmail.com>
2026-01-13 22:12:48 -0300

    refatorando o código do arquivo main.py

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from tkinter import ttk
 class App:
     def __init__(self, root):
         self.root = root
-        self.root.title("Simple Tkinter App")
-
-        self.label = Label(root, text="Hello, Tkinter!")
+        self.root.title("FinanSee - Main")
+        root.geometry("500x500+10+50") 
+        
+        self.label = Label(root, text="FinanSee !")
         self.label.pack(pady=10)
+        self.label.config(font=("Arial", 24))
 
-        self.button = Button(root, text="Click Me", command=self.on_button_click)
+        self.button = Button(root, text="Inciar", command=self.on_button_click)
         self.button.pack(pady=10)
+
+        self.tela()
 
     def on_button_click(self):
         self.label.config(text="Button Clicked!")
 
     def tela(self):
-        self.root.configure(background='lightblue')
+        self.root.configure(background='#00FF7F')
 
 if __name__ == "__main__":
     root = Tk()
